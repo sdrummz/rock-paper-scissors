dacb55a1dc1765ad4a309ed314a30fae4f5de118
szglabs <sdrummond95@gmail.com>
2020-11-25 12:41:23 -0500

    Moved JS to script.js, created style.css, added images, and built out basic UI.

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function computerSelection() {
+    let choiceArr = ['rock', 'paper', 'scissors'];
+    let choice = Math.floor(Math.random() * 3);
+    return choiceArr[choice];
+  }
+
+  let myScore = 0;
+  let computerScore = 0;
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+      computerScore++;
+      return 'You Lose! Paper beats Rock';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+      myScore++;
+      return 'You Win! Rock beats Scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+      myScore++;
+      return 'You Win! Paper beats Rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+      computerScore++;
+      return 'You Lose! Scissors beats paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+      computerScore++;
+      return 'You Lose! Rock beats Scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+      myScore++;
+      return 'You Win! Scissors beats Paper';
+    }
+  }
+
+  function game() {
+    for (let i = 0; i < 5; i++) {
+      let playerSelection = prompt('Choose rock, paper, or scissors').toLowerCase();
+      console.log(playRound(playerSelection, computerSelection()));
+      console.log('Your Score: ' + myScore + ' Computer Score: ' + computerScore);
+    }
+    if (myScore > computerScore) {
+      console.log('You win ' + myScore + ' rounds to ' + computerScore);
+    } else if (computerScore > myScore) {
+      console.log('You lose ' + computerScore + ' rounds to ' + myScore);
+    } else if (computerScore === myScore) {
+      console.log('You tied ' + computerScore + ' rounds to ' + myScore);
+    }
+  }
